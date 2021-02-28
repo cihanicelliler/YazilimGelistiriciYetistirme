d05705a9cf14ccd4d97da2705e290f3154143fce
Cihan İçelliler <icellilercihan@gmail.com>
2021-02-28 13:40:11 +0300

    Yazılım Geliştirici Yetiştirme Kampı 14. Gün

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -12,6 +14,13 @@ namespace Business.Constants
         public static string ProductsListed = "Ürünler listelendi";
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
-        public static string CategoryCountError ="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string CategoryCountError = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kayıt olundu";
+        public static string AccessTokenCreated = "Erişim tokeni oluşturuldu";
+        public static string UserAlreadyExists = "Kullanıcı zaten mevcut";
+        public static string SuccessfulLogin = "Giriş yapıldı";
+        public static string PasswordError = "Şifre yanlış";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
     }
 }
